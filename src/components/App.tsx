b643d1521910beb584c440632f018e859fe6eb8f
Gabriel Santos <gabriel.santos@kuehne-nagel.com>
2023-03-10 18:17:01 +0200

    Starting layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,26 @@
+import { Container, Typography } from '@mui/material'
+
 export default function App() {
-    return (
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        backgroundColor: 'white',
+        borderRadius: 1,
+        mt: 8,
+        mx: 'auto',
+        p: 2,
+      }}
+    >
+      <Typography
+        component="h1"
+        display="block"
+        fontWeight={600}
+        p={3}
+        variant="h5"
+      >
+        Welcome to your todo list!
+      </Typography>
+    </Container>
+  )
+}
